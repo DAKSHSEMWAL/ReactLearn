ccdee687f135b6ba645a2ff266e2366f9c5db587
DakshSemwal-zs <dakshsemwal35@gmail.com>
2023-09-20 16:09:14 +0530

    TimeLineView

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+function Timeline({ children }) {
+  return <div className="timeline">{children}</div>;
+}
+function TimelineItem({ type, title, date, description }) {
+  return (
+    <div className={`timeline-item ${type}`}>
+      <div className="timeline-item-date">{date}</div>
+      <div className="timeline-item-line"></div>
+      <div className="timeline-item-content">
+        <span className="icon">{getIcon(type)}</span>
+        <h4>{title}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function getIcon(type) {
+  switch (type) {
+    case "birthday":
+      return "🎂";
+    case "release":
+      return "🚀";
+    case "event":
+      return "📅";
+    default:
+      return "🔔";
+  }
+}
 
 function App() {
+  const [timelineData, setTimelineData] = useState([]);
+
+  useEffect(() => {
+    // Fetch the timeline data from the Ktor server
+    fetch("http://localhost:8080/timeline")
+      .then((response) => response.json())
+      .then((data) => {
+        // Set the fetched data to the component's state
+        setTimelineData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching timeline data:", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Timeline>
+      {timelineData.map((item) => (
+        <TimelineItem
+          key={item.date}
+          type={item.type}
+          title={item.title}
+          date={item.date}
+          description={item.description}
+        />
+      ))}
+    </Timeline>
   );
 }
 
